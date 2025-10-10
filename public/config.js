d5277c1d8156f390b42b9cf4cd560cfe9ad85c94
Mario Reder <mario.reder@pm.me>
2025-10-10 02:46:23 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -17,7 +17,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "Jalapeño hideout",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Rewards,Vaults,Trading,Portfolio,Markets,Leaderboard",
+  "VITE_ENABLED_MENUS": "Rewards,Vaults,Trading,Portfolio,Markets,Leaderboard,Swap",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_CAMPAIGNS": "true",
   "VITE_TELEGRAM_URL": "https://t.me/salsatekila",
